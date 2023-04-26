cf3583e2a525d5b22639bc82be4a4bdb30499007
Marius Ivanuta <mariusivanuta@yahoo.com>
2023-04-26 09:10:26 +0300

    ING add initial structure

---
+++ src/test/java/com/example/HelloWorldResourceIT.java
@@ -0,0 +1,8 @@
+package com.example;
+
+import io.quarkus.test.junit.QuarkusIntegrationTest;
+
+@QuarkusIntegrationTest
+public class HelloWorldResourceIT extends HelloWorldResourceTest {
+    // Execute the same tests but in packaged mode.
+}
